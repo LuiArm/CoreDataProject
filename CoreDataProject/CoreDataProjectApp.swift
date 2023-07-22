2b61161d3ae77107f170c2310bbf7f48d121fdb1
LuiArm <Luisarmendariz24@icloud.com>
2023-07-21 22:38:34 -0600

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by luis armendariz on 7/21/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
